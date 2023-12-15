96095e878d7dfe4172f133360bd3d6b280a72fc6
Tausif <tsftousif2@gmail.com>
2023-12-15 23:18:14 +0800

    Burger buttons in mobile screen is working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Fragment, useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
-import { Popover, Transition } from "@headlessui/react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
   ChevronDownIcon,
   HomeIcon,
@@ -11,6 +11,7 @@ import {
   PaperAirplaneIcon,
   PlayCircleIcon,
 } from "@heroicons/react/24/solid";
+import { cn } from "@/lib/utils";
 
 const products = [
   {
@@ -49,9 +50,9 @@ export default function Header() {
           {/* logo part  */}
           <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Hoichoi Fashion</span>
-            <div className="rounded-full overflow-hidden">
+            <div className="rounded-full ring ring-lime-50 overflow-hidden">
               <Image
-                className="h-12 w-12"
+                className="h-12 w-12 "
                 src="/hoichoi.jpg"
                 alt="hoichoi"
                 width="48"
@@ -96,25 +97,125 @@ export default function Header() {
                       className="ground relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-600">
-                        <item.icon className="h-6 w-6 text-[#013B94] group-hover:text-blue-600" aria-hidden="true" />
+                        <item.icon
+                          className="h-6 w-6 text-[#013B94] group-hover:text-blue-600"
+                          aria-hidden="true"
+                        />
                       </div>
                       <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-[#013B94]">
-                        {item.name}
-                        <span className="absolute inset-0"/>
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-[#013B94]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
                         </a>
-                         <p className="mt-1 text-[#013B94]">
+                        <p className="mt-1 text-[#013B94]">
                           {item.description}
-                         </p>
+                        </p>
                       </div>
                     </div>
+                  ))}
+                </div>
+                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                  {callsToAction.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013B94]"
+                    >
+                      <item.icon
+                        className="h-5 w-5 flex-none text-[#013B94]"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </a>
                   ))}
                 </div>
               </Popover.Panel>
             </Transition>
           </Popover>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Flights
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Cars
+          </a>
+          <a
+            href="#"
+            className="text-sm font-semibold leading-6 text-white ml-4"
+          >
+            Bikes
+          </a>
         </Popover.Group>
+        <div className=" hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
       </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#013B94] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Hoichoi Fashion</span>
+              <Image
+                className="h-12 w-12 "
+                src="/hoichoi.jpg"
+                alt="hoichoi"
+                width="48"
+                height="48"
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-white"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 p-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+                        Choose
+                        <ChevronDownIcon
+                          className={cn(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel as="div" className="mt-2 space-y-2">
+                        {[...products, ...callsToAction].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-800"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   );
 }
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -2,22 +2,29 @@ import React from "react";
 
 export default function Product() {
   return (
-    <div className="pt-2 flex flex-row justify-between flex-wrap gap-5 bg-gray-100">
-      <div className="max-w-sm mx-auto p-9 bg-white  rounded-xl shadow-md space-y-6">
+    <div className="pt-2 flex flex-row justify-between flex-wrap gap-5 bg-gray-100 dark:bg-gray-900">
+        {/* card starts from here  */}
+      <div className="max-w-xs mx-auto p-9 bg-white  rounded-xl shadow-md space-y-6 sm:flex sm:items-center sm:justify-between sm:space-y-2 sm:mx-3 sm:gap-5 sm:max-w-xl">
         <img
           src="https://thumbs.dreamstime.com/b/clothing-rack-15819472.jpg"
           alt=""
-          className="h-30 mx-auto overflow-hidden "
+          className="h-30 mx-auto overflow-hidden sm:mx-0 
+          ring-2 ring-purple-500 ring-offset-4 ring-offset-slate-50
+           dark:ring-offset-slate-900 transform hover:scale-105 
+           duration-500"
         />
-        <div className="text-center space-y-3">
+        <div className="text-center space-y-3  sm:space-y-5 ">
           <div>
-            <p className="text-lg text-black font-semibold">Hoichoi choluk</p>
-            <p className="text-gray-500 font-medium">Pinik hokm onk</p>
+            <p className="text-lg text-black font-semibold">Shirt</p>
+            <p className="text-gray-500 font-medium">Winter collection</p>
           </div>
-          <button className="px-4 py-1 text-sm  border border-purple-300 rounded-full text-purple-700">Visit Hoichoi Fashion</button>
+          <button className="px-4 py-1 text-sm  border border-purple-300 rounded-full text-purple-700  sm:rounded
+          hover:bg-purple-500 hover:text-white 
+           focus:outline-none
+           focus:ring-2
+          ">Buy Now!</button>
         </div>
-      </div>
-      
+      </div> 
     </div>
   );
 }
